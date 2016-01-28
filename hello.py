ff368cfef8cce46c1355ba22d47e156e66a47a78
cmis <cmis@cmis-hs05.(none)>
2016-01-28 14:36:10 +0700

    I edited my file

--- hello.py
+++ hello.py
@@ -5,3 +5,13 @@ print 34.5
 print "Staton, Hannah"
 print ("Staton", "Hannah", "15", "Americano")
 print "NOT AN ERROR"
+myLife = "BANNANANANA"
+myFruit = "Linchi"
+myHope = "To survive"
+myName = "Hannah Banana"
+
+print myLifepyt
+print myFruit
+print myHope
+print myName
+
