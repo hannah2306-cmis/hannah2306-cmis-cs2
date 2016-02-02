372898cd23422d798a076efee54d808edda41393
cmis <cmis@cmis-hs05.(none)>
2016-02-02 11:56:34 +0700

    I edited my file.

--- assignment1.py
+++ assignment1.py
@@ -20,7 +20,24 @@ yearsLeftToLive = myAgeOfDeath - myAgeInYears
 print yearsLeftToLive
 
 feetInMeter = 3.28084 
-myHeightInMeters = myHeightInMeters * feetInMeter
+myHeightInFeet = myHeightInMeters * feetInMeter
 print myHeightInMeters
 
+averageHeightAmericanInMeters = 1.597
+differenceMyHeightWithAverage = averageHeightAmericanInMeters - myHeightInMeters
+print differenceMyHeightWithAverage
 
+AreaOfSquareWithMySide = oneSideOfASquareInFeet ** 2 
+print AreaOfSquareWithMySide
+
+volumeOfCube = oneSideOfASquareInFeet ** 3 
+halfTheVolumeOfCube = volumeOfCube / 2 
+print halfTheVolumeOfCube
+
+oneNinthOfArea = AreaOfSquareWithMySide / 9
+print oneNinthOfArea
+
+print "My name is" + " " + str(myName) + "." + " " + "I am" + " " + str(myAgeInYears) + " " + "years old." + " " + "I am exactly" + " " + str(myHeightInMeters) + " " + "tall." + " " + "I am going to die when I am" + " " + str(myAgeOfDeath) + " " + "years old." + " " + "The average height for an American female at 15 years old is" + " " + str(averageHeightAmericanInMeters) + "."   
+
+print "My name is" + " " + str(myName) + "." + " " + "I am" + " " + str(myAgeInYears) + " " + "years old." + " " + "I am exactly" + " " + str(myHeightInMeters) + " " + "tall." + " " + "I am going to die when I am" + " " + str(myAgeOfDeath) + " " + "years old." + " " + "The average height for an American female at 15 years old is" + " " + str(averageHeightAmericanInMeters) + "." 
+"One side of a square in feet is" + " " + str(oneSideOfASquareInFeet) + "." + " " + "The length of a rectangle in feet is" + " " + str(lengthOfARectangleInFeet) "." + " " + "There are" + str(monthsInYear) + "in a year" + "." + " "  
