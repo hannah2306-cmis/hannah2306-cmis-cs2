aba90531c277ea6389806c9486d18b26c23363e7
cmis <cmis@cmis-hs05.(none)>
2016-02-04 14:24:53 +0700

    I edited my file.

--- assignment1.py
+++ assignment1.py
@@ -1,43 +1,44 @@
-myName = "Hannah" 
-print myName 
-myAgeInYears = 15.5 
-print myAgeInYears
-myHeightInMeters = 1.69
-print myHeightInMeters 
-oneSideOfASquareInFeet = 12 
-lengthOfARectangleInFeet = 15 
-heightOfRectangleInFeet = 5 
-print oneSideOfASquareInFeet
-print lengthOfARectangleInFeet
-print heightOfRectangleInFeet
+myName = "Hannah" #My first name
+print myName #print my first name 
+myAgeInYears = 15.5 #My age in years  
+print myAgeInYears #print my age in years
+myHeightInMeters = 1.69 #My height in meters
+print myHeightInMeters #print my height in meters 
+oneSideOfASquareInFeet = 12 #The length of one side of a square in feet 
+lengthOfARectangleInFeet = 15 #The length of a rectangle in feet
+heightOfRectangleInFeet = 5 #The height of a rectangle in feet 
+print oneSideOfASquareInFeet #print the length of one side of a square in feet
+print lengthOfARectangleInFeet #print the length of a rectangle in feet
+print heightOfRectangleInFeet #print the height of a rectangle in feet
 
-monthsInYear = 12
-myAgeInMonths = myAgeInYears * monthsInYear 
-print myAgeInMonths
+monthsInYear = 12 #How many months there are in a year 
+myAgeInMonths = myAgeInYears * monthsInYear #compute my age in months 
+print myAgeInMonths #print my age in months
 
-myAgeOfDeath = 101
-yearsLeftToLive = myAgeOfDeath - myAgeInYears 
-print yearsLeftToLive
+myAgeOfDeath = 101 #My age when I die
+yearsLeftToLive = myAgeOfDeath - myAgeInYears #compute how many years I have left to live
+print yearsLeftToLive #print the years I have left to live
 
-feetInMeter = 3.28084 
-myHeightInFeet = myHeightInMeters * feetInMeter
-print myHeightInMeters
+feetInMeter = 3.28084 #How many feet in a meter 
+myHeightInFeet = myHeightInMeters * feetInMeter #compute my height in feet
+print myHeightInMeters #print my height in meters 
 
-averageHeightAmericanInMeters = 1.597
-differenceMyHeightWithAverage = averageHeightAmericanInMeters - myHeightInMeters
-print differenceMyHeightWithAverage
+averageHeightAmericanInMeters = 1.597 #The average height of an American girl at the age of 15
+differenceMyHeightWithAverage = averageHeightAmericanInMeters - myHeightInMeters #compute the difference between my height with the average American girl height 
+print differenceMyHeightWithAverage #print the difference between my height with the average American girl height 
 
-AreaOfSquareWithMySide = oneSideOfASquareInFeet ** 2 
-print AreaOfSquareWithMySide
+AreaOfSquareWithMySide = oneSideOfASquareInFeet ** 2 #compute the area of my square  
+print AreaOfSquareWithMySide #print the area of my square  
 
-volumeOfCube = oneSideOfASquareInFeet ** 3 
-halfTheVolumeOfCube = volumeOfCube / 2 
-print halfTheVolumeOfCube
+volumeOfCube = oneSideOfASquareInFeet ** 3 #compute the volume of my cube
+halfTheVolumeOfCube = volumeOfCube / 2 #compute the half of the volume of my cube
+print halfTheVolumeOfCube #print half of the volume of my cube
 
-oneNinthOfArea = AreaOfSquareWithMySide / 9
-print oneNinthOfArea
+oneNinthOfArea = AreaOfSquareWithMySide / 9 #compute one ninth of the area of my square
+print oneNinthOfArea #print one ninth of the area of my square
 
-print "My name is" + " " + str(myName) + "." + " " + "I am" + " " + str(myAgeInYears) + " " + "years old." + " " + "I am exactly" + " " + str(myHeightInMeters) + " " + "tall." + " " + "I am going to die when I am" + " " + str(myAgeOfDeath) + " " + "years old." + " " + "The average height for an American female at 15 years old is" + " " + str(averageHeightAmericanInMeters) + "."   
+print "My name is" + " " + str(myName) + "." + " " + "I am" + " " + str(myAgeInYears) + " " + "years old." + " " + "I am exactly" + " " + str(myHeightInMeters) + " " + "tall." + " " + "I am going to die when I am" + " " + str(myAgeOfDeath) + " " + "years old." + " " + "The average height for an American female at 15 years old is" + " " + str(averageHeightAmericanInMeters) + "." #print a message using 5 of the variables I've created above
 
-print "My name is" + " " + str(myName) + "." + " " + "I am" + " " + str(myAgeInYears) + " " + "years old." + " " + "I am exactly" + " " + str(myHeightInMeters) + " " + "tall." + " " + "I am going to die when I am" + " " + str(myAgeOfDeath) + " " + "years old." + " " + "The average height for an American female at 15 years old is" + " " + str(averageHeightAmericanInMeters) + "." 
-"One side of a square in feet is" + " " + str(oneSideOfASquareInFeet) + "." + " " + "The length of a rectangle in feet is" + " " + str(lengthOfARectangleInFeet) "." + " " + "There are" + str(monthsInYear) + "in a year" + "." + " "  
+print "My name is" + " " + str(myName) + "." + " " + "I am" + " " + str(myAgeInYears) + " " + "years old." + " " + "I am exactly" + " " + str(myHeightInMeters) + " " + "tall." + " " + "I am going to die when I am" + " " + str(myAgeOfDeath) + " " + "years old." + " " + "The average height for an American female at 15 years old is" + " " + str(averageHeightAmericanInMeters) + "." + " " + "One side of a square in feet is" + " " + str(oneSideOfASquareInFeet) + "." + " " + "The length of a rectangle in feet is" + " " + str(lengthOfARectangleInFeet) + "." + " " + "There are" + " " + str(monthsInYear) + " " + "months in a year" + "." + " " + "The height of a rectangle in feet is" + " " + str(heightOfRectangleInFeet) + "." + " " + "My age in months is" + " " + str(myAgeInMonths) + "." #print a second message using 5 more of the variables I've created. 
+winkyface = ";)" #winking face emoji in string form
+print winkyface * 10000 #print the winking face emoji 10000 times  
