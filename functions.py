513f01347c677f59da1d5c3f9e004ab3e7143a1a
cmis <cmis@cmis-hs05.(none)>
2016-02-09 14:09:55 +0700

    I created a file

---
+++ functions.py
@@ -0,0 +1,14 @@
+def add(a,b): 
+	return a + b
+def sub(a,b): 
+	return a - b
+def mul(a,b): 
+	return a * b
+def div(a,b): 
+	return a / b
+def hours_from_seconds(a): 
+	return a / 3600 
+def circle_area( a) 
+	return 
+def sphere_volume ( a ) 
+
