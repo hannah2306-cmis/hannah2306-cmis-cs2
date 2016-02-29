9d0bbc160c6a9e7ca3cc32075eca5ff2ed7a1650
cmis <cmis@cmis-hs05.(none)>
2016-02-29 15:00:15 +0700

    I edited my file.

--- structure.py
+++ structure.py
@@ -1,7 +1,7 @@
 #Define the critical functions used in the program
 # These function should almost always return a value 
 
-def mul(x,y): 
+def bothdaughtershairscombined(x,y): 
 	return 2 * (x + y)  
 
 def output(name, x, y, z):
@@ -9,24 +9,20 @@ def output(name, x, y, z):
 HEY GUYS
 My name is {}. 
 I am your nurse. 
-Your baby girl might have {} hairs on the top of her head. 
-Oops. We just detected she might have {} hairs on her face as well. 
-We are afraid that if she has more than {} hairs on her face and more than {} hairs on her head... 
-She could be diagnosed with hypertrichosis. 
-However, if are able to wax and shave {} amount of hair on her face. 
-We might get all but {} hairs on her face. 
-I'm sorry but that is the best we can do. 
-Hope your daughter doesn't grow up and get bullied.   
+Your baby girl might have {} hairs on her face. 
+We are afraid that if she has more than {} hairs on her face... 
+Then she could be diagnosed with hypertrichosis.  
+So if we add both of your daughter's amount of hair on the head and face, then we have {} amount of hair for both daughters together. 
+This means really need to try and shave your daughter's face with hypertrichosis to have an even number of hairs for both children. We should end up with an easy 100.    
 """.format(name, x, y, z)
 	return out
 
 def main():
 	name= raw_input("What's your name?: ")
-	x = raw_input("Type your brother's age: ")
-	y = raw_input("Type your age: ") 
-	z = add(int(x), int(y))
+	x = raw_input("Type your daughter's amount of hair on her face with hypertrichosis: ")
+	y = raw_input("Type the amount of hair she should have on her face: ") 
+	z = bothdaughtershairscombined(int(x), int(y))
 	out = output(name, x, y, z)
 	print out 
 
 main() 
-
