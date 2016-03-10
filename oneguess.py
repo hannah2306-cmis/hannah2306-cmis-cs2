5fd74ae11e87eec4ba8959c6f8e2c6a598db6686
cmis <cmis@cmis-hs05.(none)>
2016-03-10 14:36:46 +0700

    I created this file.

---
+++ oneguess.py
@@ -0,0 +1,16 @@
+def output(minimumNumber, maximumNumber, beginRange, endRange, targetNumber)
+	out = """
+What is the minimum nmber? {}
+What is the maximum number? {}
+I'm thinking of a number from {} to {}.
+What do you think it is?: {}
+
+The target was {}.
+Your guess was {}.
+That's under by {}.
+""".format(
+
+def main():
+	minimumNumber = raw_input("What is the minimum number?: ") 
+	maximumNumber = raw_input("What is the maximum number?: ") 
+	beginRange = 
