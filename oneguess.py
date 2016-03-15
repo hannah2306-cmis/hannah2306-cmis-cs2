3a5c467836d4d9c498ea8bc0bfd1d3edad6b7335
cmis <cmis@cmis-hs05.(none)>
2016-03-15 09:57:01 +0700

    I edited this file.

--- oneguess.py
+++ oneguess.py
@@ -1,16 +1,36 @@
-def output(minimumNumber, maximumNumber, beginRange, endRange, targetNumber)
+def output(minimumNumber, maximumNumber, targetNumber):
 	out = """
-What is the minimum nmber? {}
+What is the minumum number? {}
 What is the maximum number? {}
-I'm thinking of a number from {} to {}.
-What do you think it is?: {}
-
-The target was {}.
-Your guess was {}.
-That's under by {}.
-""".format(
+""".format(minimumNumber, maximumNumber)
+    return out
 
 def main():
 	minimumNumber = raw_input("What is the minimum number?: ") 
 	maximumNumber = raw_input("What is the maximum number?: ") 
-	beginRange = 
+main()
+
+def output(minimumNumber, maximumNumber)
+    out = """
+I'm thinkng of a number from {} to {}.
+""".format(minmumNumber, maximumNumber) 
+
+def main()
+     guessing = raw_input("What do you think it is?: ")
+main()
+
+import random 
+
+def randomize(beginingNumber, endingNumber)  
+    return random.randint(beginingNumber, endingNumber)
+
+def output(targetNumber, guess):
+    out = """
+
+    print "I'm thinking of a number from 1-10." 
+	targetNumber = raw_input("What do you think it is?: ")
+    out = output(minimumNumber, maximumNumber, targetNumber) 
+    print out
+
+main()
+
