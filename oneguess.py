f2c92eef3e7a323fd162398e7986ce76404547e1
cmis <cmis@cmis-hs05.(none)>
2016-03-16 11:56:44 +0700

    I edited my file.

--- oneguess.py
+++ oneguess.py
@@ -1,36 +1,64 @@
-def output(minimumNumber, maximumNumber, targetNumber):
+def output(minimumNumber, maximumNumber, targetNumber, guess):
 	out = """
 What is the minumum number? {}
 What is the maximum number? {}
+I'm thinkng of a number from {} to {}.
 """.format(minimumNumber, maximumNumber)
     return out
-
-def main():
-	minimumNumber = raw_input("What is the minimum number?: ") 
-	maximumNumber = raw_input("What is the maximum number?: ") 
-main()
 
 def output(minimumNumber, maximumNumber)
     out = """
 I'm thinkng of a number from {} to {}.
 """.format(minmumNumber, maximumNumber) 
 
-def main()
-     guessing = raw_input("What do you think it is?: ")
-main()
+def guessing = raw_input("What do you think it is?: ")
 
 import random 
 
-def randomize(beginingNumber, endingNumber)  
-    return random.randint(beginingNumber, endingNumber)
+def randomize(minimum, maximum)  
+    return random.randint(minimum, maximum)
 
 def output(targetNumber, guess):
     out = """
-
+The target was {}.
+Your guess was {}.
+That's under by {}.
+""".format(
+ 
     print "I'm thinking of a number from 1-10." 
 	targetNumber = raw_input("What do you think it is?: ")
     out = output(minimumNumber, maximumNumber, targetNumber) 
     print out
 
-main()
+
+def main():
+    minimumNumber = raw_input("What is the minimum number?: ") 
+	maximumNumber = raw_input("What is the maximum number?: ")
+    targetNumber = randomize(minimumNumber, maximumNumber) 
+    output = """ 
+I'm think of a number from {} to {}.
+""".format(minimumNumber, maximumNumber)
+    print output
+    guessing = raw_input("What do you think it is?: ")   
+    if targetNumber == guessing: 
+        print """
+The target was {}.
+Your guess was {}.
+That's right! You're a psychic!
+""".format(targetNumber, guessing) 
+    elif targetNumber < guessing: 
+    difference = guessing - targetNumber 
+    
+
+
+
+    out = output(int(minimumNumber), int(maximumNumber), int(output), int(guess))
+    print out 
+main() 
+
+def differenceOfguessingandtargetNumber(g, t)
+    return 
+difference = differenceofguessingandtargetnumber(guess, target) 
+
+
 
