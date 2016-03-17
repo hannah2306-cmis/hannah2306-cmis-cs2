913354e217921aa28d9a3e9c648b031a24d4cb65
cmis <cmis@cmis-hs07.(none)>
2016-03-17 14:36:51 +0700

    I edited this file.

--- oneguess.py
+++ oneguess.py
@@ -1,40 +1,11 @@
-def output(minimumNumber, maximumNumber, targetNumber, guess):
-	out = """
-What is the minumum number? {}
-What is the maximum number? {}
-I'm thinkng of a number from {} to {}.
-""".format(minimumNumber, maximumNumber)
-    return out
-
-def output(minimumNumber, maximumNumber)
-    out = """
-I'm thinkng of a number from {} to {}.
-""".format(minmumNumber, maximumNumber) 
-
-def guessing = raw_input("What do you think it is?: ")
-
 import random 
-
-def randomize(minimum, maximum)  
-    return random.randint(minimum, maximum)
-
-def output(targetNumber, guess):
-    out = """
-The target was {}.
-Your guess was {}.
-That's under by {}.
-""".format(
- 
-    print "I'm thinking of a number from 1-10." 
-	targetNumber = raw_input("What do you think it is?: ")
-    out = output(minimumNumber, maximumNumber, targetNumber) 
-    print out
-
+import math
+from random import randint
 
 def main():
     minimumNumber = raw_input("What is the minimum number?: ") 
-	maximumNumber = raw_input("What is the maximum number?: ")
-    targetNumber = randomize(minimumNumber, maximumNumber) 
+    maximumNumber = raw_input("What is the maximum number?: ")
+    targetNumber = random.randint(minimumNumber, maximumNumber) 
     output = """ 
 I'm think of a number from {} to {}.
 """.format(minimumNumber, maximumNumber)
@@ -47,18 +18,17 @@ Your guess was {}.
 That's right! You're a psychic!
 """.format(targetNumber, guessing) 
     elif targetNumber < guessing: 
-    difference = guessing - targetNumber 
-    
-
-
-
-    out = output(int(minimumNumber), int(maximumNumber), int(output), int(guess))
-    print out 
+        difference = guessing - targetNumber 
+        print """
+The target was {}.
+Your guess was {}.
+That's under by {}. 
+""".format(targetNumber, guessing, difference) 
+    elif targetNumber > guessing:
+        difference = abs(targetNumber - guessing)
+        print """
+The target was {}.
+Your guess was {}.
+That's over by {}.
+""".format(targetNumber, guessing, difference) 
 main() 
-
-def differenceOfguessingandtargetNumber(g, t)
-    return 
-difference = differenceofguessingandtargetnumber(guess, target) 
-
-
-
