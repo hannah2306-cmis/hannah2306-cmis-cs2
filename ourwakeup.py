e33b3a2926371fb9b5f9670a054550a06fbd20f7
cmis <cmis@cmis-hs22.(none)>
2016-04-18 09:56:20 +0700

    I edited this file

---
+++ ourwakeup.py
@@ -0,0 +1,36 @@
+
+print "This program will ask for 5 integer or float values. It will calculate the average of all values from 0 inclusive to 10 exclusive. It will print out whether the resulting average is even or odd." 
+
+def main(): 
+
+    0thnumber = raw_input("n0: ") 
+    if float(0thnumber) < 0 or float(0thnumber) >= 10 
+        floated0thnumber = float(0thnumber) 
+        print floated0thnumber " is out of range."
+
+	else sumofnumbers = float(0thnumber)
+	else Anumberofnumbers = 1
+     
+    firstnumber = raw_input("n1: ") 
+        if float(firstnumber) = 0 or float(firstnumber) >= 10
+            floatedfirstnumber = float(firstnumber) 
+                print floatedfirstnumber " is out of range." 
+
+        else 
+            if float(0thnumber) >= 0 or float(0thnumber) < 10
+                sumofnumbers = float(0thnumber) + float(firstnumber) 
+                Bnumberofnumbers = Anumberofnumbers + 1
+    secondnumber = raw_input("n2: ")
+        if float(secondnumber) < 0 or float(secondnumber) >= 10
+             floatedsecondnumber = float(secondnumber)
+                print floatedsecondnumber " is out of range." 
+    thirdnumber = raw_input("n3: ")
+        if float(thirdnumber) < 0 or float(thirdnumber) >= 10
+            floatedthirdnumber = float(thirdnumber)
+                print floatedthirdnumber " is out of range."
+    fourthnumber = raw_input("n4: ") 
+        if float(fourthnumber) < 0 or float(fourthnumber) >= 10 
+            floatedfourthnumber = float(fourthnumber)
+                print floatedfourthnumber " is out of range." 
+
+
