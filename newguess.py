7355b17b3b8636f219668b68a78ae6f2f41bc385
cmis <cmis@cmis-hs23.(none)>
2016-05-06 11:53:00 +0700

    I edited this file

--- newguess.py
+++ newguess.py
@@ -3,8 +3,26 @@ import random
 def rounds(numberrounds, c): 
 #c = number of rounds you got correct
 	if numberrounds == 0: 
-		return "You got " + str(c) "rounds correct." 
+		return "You got " + str(c) + "rounds correct." 
 	else:
-
-#random.randint(1, 100) 
-
+		print "Starting round" + str(numberrounds)
+		correct += guess(random.randint(1, 100), 6)
+		return rounds(numberrounds - 1, correct)
+def guess(random, attempt): 
+	input = int(raw_input("Guess a number: "))
+	if input == random: 
+		print "That's correct!"
+		return 1 
+	elif attempts == 1: 
+		print "The answer was " + str(random) + "You aren't that good at this game"
+		return 0
+	elif input > random: 
+		print "That's too high." 
+		return guess(random, attempt -1) 
+	elif input < random: 
+		print "That's too low."
+		return guess(random, attempt -1)  
+def main(): 
+	result = rounds(1,0) 
+	print result
+main()
