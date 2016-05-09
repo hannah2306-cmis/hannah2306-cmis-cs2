7e6bdb69bb1cab4df50f890436e55e6f655b608a
cmis <cmis@cmis-hs23.(none)>
2016-05-09 14:36:15 +0700

    I am finishing this

--- conditionals.py
+++ conditionals.py
@@ -5,7 +5,18 @@ import random
 #I have shown the strength of each weapon you can choose from. The scale is on a 1-10 rating of each weapon. As demonstrated below, the sword is the best choice. 
 #I have shown the different kinds of attacks that you should look out for from the dragon. The moves the dragon uses on you is on a scale from 1-10. 
 
-def yourdecision(weapon) 
+def main(): 
+	area = raw_input("Where are you?: ") 
+	dominantarm = raw_input("What is your dominant hand?: ")
+	energy = raw_input("Did you eat enough?: ") 
+	output = """
+You are in the {}!
+You should be in an area where 
+	 
+aliceweapon(choice)
+
+
+def yourdecision(weapon): 
 	
 	if weapon == "torch": 
 		strength = 0 
@@ -18,9 +29,9 @@ def yourdecision(weapon)
 	elif weapon == "sword": 
 		strength = 10 
 
-
 	return strength * random.random() 
-def dragonattacks(move)
+
+def dragonattacks(move):
 
 	if move == "breathing fire":
 		enemystrength = 10
@@ -37,8 +48,27 @@ def dragonattacks(move)
 		enemystrength = random.randint(0.10) 
 	return strength * random.random()
 
-if yourstrength > enemystrength 
-	def main():
+def aliceweapon(choice):
+	if choice == torch:
+		print "You are going to lose this battle!"
+	elif choice == stick:
+		print "You are going to get injured!" 
+	elif choice == whip:
+		print "You can stike the dragon only a few times. But he will still beat you!" 
+	elif choice == bowling ball: 
+		print "You are going to do pretty well! If you try hard enough...you might win! YAY!" 
+	elif choice == sword: 
+		print "You've got this, Alice! You are capable of beating this dragon! I believe in you!"
+  
+def Alice(position):
+	if position == wide legs 
+		print "You are strong enough to fight this dragon."
+	if position == feet together
+		print "You are too weak to fight this dragon." 
+	if position == right foot in front
+		
+			 
+def main():
     minimumNumber = int(raw_input("What is the minimum number?: ")) 
     maximumNumber = int(raw_input("What is the maximum number?: "))
     targetNumber = random.randint(minimumNumber, maximumNumber) 
@@ -67,7 +97,7 @@ The target was {}.
 Your guess was {}.
 That's over by {}.
 """.format(targetNumber, guessing, difference) 
+aliceweapon(choice)
 main() 
 	   
-def main(): 
 
