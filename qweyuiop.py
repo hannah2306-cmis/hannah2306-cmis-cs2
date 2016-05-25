b0b9dce5d509cdb1a73f3c359c810a527aa26543
cmis <cmis@cmis-hs23.(none)>
2016-05-25 11:15:45 +0700

    done

---
+++ qweyuiop.py
@@ -0,0 +1,11 @@
+def function4(a, b,c):
+	if a>b and a>c:
+		return 1 + function4(b+1,c,a)
+	elif b>a or b>c:
+		return 1 - function4(b-1,c,a)
+	else:
+		return a+b+c
+
+print function4(1,2,3) 
+print function4(3,2,1)
+print function4(1,3,2)
